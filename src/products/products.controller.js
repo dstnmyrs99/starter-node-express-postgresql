9ac7774bc724b270f0c113aa9ed7cf6a1ceedd1e
kchia <kchia87@gmail.com>
2020-12-02 13:11:24 -1000

    add join base queries and query builder functions

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -6,10 +6,36 @@ async function productExists(req, res, next) {
   const { productId } = req.params;
   if (!productId) return next(error);
 
-  let product = await ProductsService.getProductById(productId);
+  let { included = [] } = req.query;
+  included = Array.isArray(included) ? included : [included];
+
+  let product;
+
+  if (included.length === 0) {
+    product = await ProductsService.getProductById(productId);
+  }
+
+  // /products/:productId?included=categories
+  if (included.includes("categories") && !included.includes("suppliers")) {
+    product = await ProductsService.getProductByIdWithCategories(productId);
+  }
+
+  // /products/:productId?included=suppliers
+  if (!included.includes("categories") && included.includes("suppliers")) {
+    product = await ProductsService.getProductByIdWithSuppliers(productId);
+  }
+
+  // /products/:productId?included=categories&included=suppliers
+  if (included.includes("categories") && included.includes("suppliers")) {
+    product = await ProductsService.getProductByIdWithCategoriesAndSuppliers(
+      productId
+    );
+  }
 
   if (!product) return next(error);
+
   res.locals.product = product;
+
   next();
 }
 
@@ -18,6 +44,7 @@ function queryParametersAreValid(req, res, next) {
     get_out_of_stock_count: ["1"],
     get_prices_summary_stats: ["1"],
     get_total_weight: ["1"],
+    included: ["categories"],
   };
 
   for (const [key, value] of Object.entries(req.query)) {
@@ -32,16 +59,20 @@ function queryParametersAreValid(req, res, next) {
     }
   }
 
-  const {
+  let {
     get_out_of_stock_count: getOutOfStockCount = "",
     get_prices_summary_stats: getPricesSummaryStats = "",
     get_total_weight: getTotalWeight = "",
+    included = [],
   } = req.query;
+
+  included = Array.isArray(included) ? included : [included];
 
   res.locals.query = {
     getOutOfStockCount,
     getPricesSummaryStats,
     getTotalWeight,
+    included,
   };
 
   next();
@@ -54,7 +85,12 @@ function read(req, res, next) {
 
 async function list(req, res, next) {
   const { query } = res.locals;
-  const { getOutOfStockCount, getPricesSummaryStats, getTotalWeight } = query;
+  const {
+    getOutOfStockCount,
+    getPricesSummaryStats,
+    getTotalWeight,
+    included,
+  } = query;
 
   let products;
 
@@ -75,6 +111,11 @@ async function list(req, res, next) {
   // /products?get_total_weight=1
   if (getTotalWeight === "1") {
     products = await ProductsService.getTotalWeightOfEachProduct();
+  }
+
+  // /products?get_total_weight=1&included=categories
+  if (getTotalWeight === "1" && included.includes("categories")) {
+    products = await ProductsService.getTotalWeightOfProductsByCategory();
   }
   res.json({ data: products });
 }
--- src/products/products.service.js
+++ src/products/products.service.js
@@ -2,6 +2,21 @@ const knex = require("../db/connection");
 
 const products = knex("products");
 
+const productsSuppliersJoin = knex("products as p").join(
+  "suppliers as s",
+  "p.supplier_id",
+  "s.supplier_id"
+);
+
+const productsCategoriesJoin = knex("products as p")
+  .join("products_categories as pc", "p.product_id", "pc.product_id")
+  .join("categories as c", "pc.category_id", "c.category_id");
+
+const productsCategoriesSuppliersJoin = knex("products as p")
+  .join("products_categories as pc", "p.product_id", "pc.product_id")
+  .join("categories as c", "pc.category_id", "c.category_id")
+  .join("suppliers as s", "p.supplier_id", "s.supplier_id");
+
 const getAllProducts = () => products.select("*");
 
 const getProductById = productId =>
@@ -35,10 +50,43 @@ const getTotalWeightOfEachProduct = () =>
     .groupBy("product_title", "product_sku")
     .orderBy("total_weight_in_lbs");
 
+const getProductByIdWithCategories = productId =>
+  productsCategoriesJoin
+    .select("p.*", "c.*")
+    .where({ "p.product_id": productId })
+    .first();
+
+const getProductByIdWithSuppliers = productId =>
+  productsSuppliersJoin
+    .select("p.*", "s.*")
+    .where({ "p.product_id": productId })
+    .first();
+
+const getProductByIdWithCategoriesAndSuppliers = productId =>
+  productsCategoriesSuppliersJoin
+    .select("p.*", "c.*", "s.*")
+    .where({ "p.product_id": productId })
+    .first();
+
+const getTotalWeightOfProductsByCategory = () =>
+  productsCategoriesJoin
+    .select(
+      "c.category_name",
+      knex.raw(
+        "sum(p.product_weight_in_lbs * p.product_quantity_in_stock) as total_weight_by_category"
+      )
+    )
+    .groupBy("c.category_name")
+    .orderBy("total_weight_by_category");
+
 module.exports = {
   getAllProducts,
   getProductById,
   getOutOfStockProductsCount,
   getMinMaxAveragePricesOfProductsBySupplier,
   getTotalWeightOfEachProduct,
+  getProductByIdWithCategories,
+  getProductByIdWithSuppliers,
+  getProductByIdWithCategoriesAndSuppliers,
+  getTotalWeightOfProductsByCategory,
 };
