20942b9ebfc055447a9cde0401d6df87cd3318b7
Dustin Myers <dstnmyrs99@yahoo.com>
2021-03-03 10:43:22 -0600

    finished server

--- src/categories/categories.service.js
+++ src/categories/categories.service.js
@@ -2,6 +2,9 @@ const knex = require("../db/connection");
 
 const getAllCategories = () => knex("categories").select("*");
 
+const getCount = () => knex("categories").count("*");
+
 module.exports = {
   getAllCategories,
+  getCount
 };
